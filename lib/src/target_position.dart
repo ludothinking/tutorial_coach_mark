48fb3abfd3045ec36b508148302fa1e4eab4410d
Fabricio Caneva <hardlymersk@gmail.com>
2020-10-15 14:02:14 -0300

    add custom operators

--- lib/src/target_position.dart
+++ lib/src/target_position.dart
@@ -1,8 +1,31 @@
 import 'dart:ui';
 
+import 'package:flutter/widgets.dart';
+
 class TargetPosition {
   final Size size;
   final Offset offset;
 
   TargetPosition(this.size, this.offset);
+
+  TargetPosition operator -(TargetPosition other) {
+    return TargetPosition(
+      size - Offset(other.size.width, other.size.height),
+      offset - other.offset,
+    );
+  }
+
+  TargetPosition operator +(TargetPosition other) {
+    return TargetPosition(
+      size + Offset(other.size.width, other.size.height),
+      offset + other.offset,
+    );
+  }
+
+  TargetPosition operator *(double operand) {
+    return TargetPosition(
+      size * operand,
+      offset * operand,
+    );
+  }
 }
